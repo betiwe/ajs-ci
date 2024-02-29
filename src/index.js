8a8e50c1888443a851b7d95bcb86732cde75ce9e
“betiwe” <betiwe@yandex.ru>
2024-02-29 19:28:55 +0300

    update

--- src/index.js
+++ src/index.js
@@ -1,11 +1,9 @@
 export default function howHealthy(object){
 	if (object.health > 50) {
 		return 'healthy';
-	}
-	if (object.health <= 50 && object.health >= 15) {
+	} else if (object.health <= 50 && object.health >= 15) {
 		return 'wounded';
-	}
-	if (object.health < 15) {
+	} else {
 		return 'critical';
 	}
 }
